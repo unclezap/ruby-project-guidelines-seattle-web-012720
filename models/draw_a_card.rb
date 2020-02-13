1608a4e1802d46f03497ecffc02c814a767a9a67
unclezap <zeb.blackwell@gmail.com>
2020-02-13 14:51:19 -0800

    card drawing function

--- models/draw_a_card.rb
+++ models/draw_a_card.rb
@@ -1,14 +1,20 @@
 class DrawACard
     def self.run(game)
         cards = game.shuffles
-        binding.pry
-        cards_in_deck = cards.find_all {|card| card[in_deck] == true}
-        binding.pry
+        cards_in_deck = cards.find_all do |card|
+            card[:in_deck] == true
+        end
+        # binding.pry
         if cards_in_deck == nil
             binding.pry
             WinLoseDraw.run(game)
         end
-        
-        CalculatePossibleSet.run
+        cards_in_deck.first[:on_board] = true
+        cards_in_deck.first[:in_deck] = false
+        cards_on_board = cards.find_all do |card|
+            card[:on_board] == true
+        end
+        binding.pry
+        CalculatePossibleSet(cards_on_board).run
     end
 end
