11d1917c240292cd3fd012eef4e4a4af3faabc4c
unclezap <zeb.blackwell@gmail.com>
2020-02-14 08:52:39 -0800

    Looks like I had some spare code sitting around in my run file. Got rid of it.

--- bin/run.rb
+++ bin/run.rb
@@ -1,15 +1,6 @@
 require_relative '../config/environment'
 require 'timeout'
 
-
-x = Time.now
-gets
-input = "no"
-while Time.now < x + 15 || input == "no"
-    puts "input"
-    input = gets
-end
-
 binding.pry
 StartMenu.run
 
