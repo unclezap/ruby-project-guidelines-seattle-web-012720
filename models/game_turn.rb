fdc06d0e219388a576a5c810c8d6feb98c413e16
unclezap <zeb.blackwell@gmail.com>
2020-02-13 22:20:30 -0800

    GameTurn function finished.  Master algorithm for playing the game.

--- models/game_turn.rb
+++ models/game_turn.rb
@@ -1,30 +1,25 @@
 class GameTurn
     def self.run(game)
         draw_results = DrawACard.run(game)
-        # binding.pry
         board = draw_results[:cards_on_board]
         winning_sets = draw_results[:takes]
-        #[    Wait Until Unless function
+        #Wait Until Unless function
         x = Time.now
         y = 0.8 * game[:difficulty]
         z = 0.4 * y * rand()
         rand_time = y + z.round()
         begin
             status = Timeout::timeout(rand_time) do
+                DisplayBoard.run(board)
                 puts "Choose your cards: "
-                cards = gets.chomp
+                guess = gets.chomp
+                if guess
+                    PlayerTake.run(game, winning_sets, guess)
+                end
             end
         rescue Timeout::Error
-                puts "Here is where your computer takes goes"
-                binding.pry
-        end 
-        # # puts "Got: #{status}"
-        # rescue Timeout::Error
-        # binding.pry
-        
-        # end
-        # ComputerTake.run(game, set) || PlayerTake.run(game, set)
-        
+                ComputerTake.run(game, winning_sets)
+        end        
         GameTurn.run
     end
 end
